0805bcc15c309472a6cf2ae2fcc034eb9503c0a7
src_resources <src.res.211@gmail.com>
2025-02-03 10:55:33 +0800

    Initialise the repository

---
+++ src/main/kotlin/top/srcres258/tutorialmod/TutorialModClient.kt
@@ -0,0 +1,7 @@
+package top.srcres258.tutorialmod
+
+import net.fabricmc.api.ClientModInitializer
+
+object TutorialModClient : ClientModInitializer {
+    override fun onInitializeClient() {}
+}
---
+++ src/main/kotlin/top/srcres258/tutorialmod/TutorialModDataGenerator.kt
@@ -0,0 +1,9 @@
+package top.srcres258.tutorialmod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object TutorialModDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
