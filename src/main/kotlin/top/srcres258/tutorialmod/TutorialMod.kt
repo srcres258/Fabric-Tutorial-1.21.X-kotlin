a45ce440e2693e289624b1838133791b2f4e4fef
src_resources <src.res.211@gmail.com>
2025-02-03 17:10:24 +0800

    Add items

--- src/main/kotlin/top/srcres258/tutorialmod/TutorialMod.kt
+++ src/main/kotlin/top/srcres258/tutorialmod/TutorialMod.kt
@@ -1,16 +1,15 @@
 package top.srcres258.tutorialmod
 
 import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import top.srcres258.tutorialmod.item.ModItems
 
 object TutorialMod : ModInitializer {
 	const val MOD_ID = "tutorialmod"
-    private val LOGGER = LoggerFactory.getLogger(MOD_ID)
+	val LOGGER: Logger = LoggerFactory.getLogger(MOD_ID)
 
 	override fun onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-		LOGGER.info("Hello Fabric world!")
+		ModItems.registerModItems()
 	}
 }
---
+++ src/main/kotlin/top/srcres258/tutorialmod/item/ModItems.kt
@@ -0,0 +1,27 @@
+package top.srcres258.tutorialmod.item
+
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents
+import net.minecraft.item.Item
+import net.minecraft.item.ItemGroups
+import net.minecraft.registry.Registries
+import net.minecraft.registry.Registry
+import net.minecraft.util.Identifier
+import top.srcres258.tutorialmod.TutorialMod
+
+object ModItems {
+    val PINK_GARNET: Item = registerItem("pink_garnet", Item(Item.Settings()))
+    val RAW_PINK_GARNET: Item = registerItem("raw_pink_garnet", Item(Item.Settings()))
+
+    private fun registerItem(name: String, item: Item) =
+        Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name), item)
+
+    fun registerModItems() {
+        TutorialMod.LOGGER.info("Registering Mod Items for ${TutorialMod.MOD_ID}")
+
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+            .register { entries ->
+                entries.add(PINK_GARNET)
+                entries.add(RAW_PINK_GARNET)
+            }
+    }
+}
